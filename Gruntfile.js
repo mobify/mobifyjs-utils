89d52e45b6a27645ba6e13e96686256157b0639c
Shawn Jansepar <shawn@mobify.com>
2014-03-03 18:17:25 -0800

    Changing version, updating tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -129,6 +129,8 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-express');
     grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-saucelabs');
 
+    grunt.registerTask('saucelabs', ['test', 'saucelabs-qunit']);
     grunt.registerTask('test', ['express', 'qunit']);
 };
